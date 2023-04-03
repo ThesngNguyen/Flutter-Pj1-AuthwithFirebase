e2b00c197cd0f71887e6b907211a8b7ceb3e9a8d
ThesngNguyen <ncthang151002@gmail.com>
2023-04-04 02:36:19 +0700

    Small change to methods

--- lib/Authenticate/auth.dart
+++ lib/Authenticate/auth.dart
@@ -1,13 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
   final FirebaseAuth _auth = FirebaseAuth.instance; // Create firebase auth instance
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  
 
-  //Methods create Account
-  Future<void> registerWithEmailAndPassword(
+  /// Methods create Account
+  Future<void> signUpWithEmailAndPassword(
       String email, String password) async {
+        
     final user = await _auth.createUserWithEmailAndPassword(
-        email: email, password: password);
+      email: email, 
+      password: password);
   }
 
   //Methods login Account
