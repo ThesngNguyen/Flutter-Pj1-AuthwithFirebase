e79b3e79b36873214bb036eeec569865a6dc7dce
ThesngNguyen <ncthang151002@gmail.com>
2023-04-08 01:49:03 +0700

    Small change to togglePages Signup and SignIn

--- lib/ViewPage/Signin_Signup.dart
+++ lib/ViewPage/Signin_Signup.dart
@@ -14,6 +14,7 @@ class _SignInOrSignUpState extends State<SignInOrSignUp> {
 
   bool showSigninPage = true;
 
+  ///Toggle Pages
   void togglePages() {
     setState(() {
       showSigninPage = !showSigninPage;
